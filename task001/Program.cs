f763fda981fad634f9579621bfbc22c8e4bc1cd7
Eugene <zh-gen@mail.ru>
2024-01-30 15:19:00 +0300

    square

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пишем прогу, которая возводит число в квадрат на выходе
+//  1. На вход принимаем число
+//  2. Возводим в квадрат
+
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine(square);
