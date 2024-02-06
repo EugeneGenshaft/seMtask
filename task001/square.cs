311e0bdd39b6952ef0d96c9894aadec8b88c02c3
Eugene <zh-gen@mail.ru>
2024-02-06 15:10:03 +0300

    renamed task001

---
+++ task001/square.cs
@@ -0,0 +1,9 @@
+﻿// Пишем прогу, которая возводит число в квадрат на выходе
+//  1. На вход принимаем число
+//  2. Возводим в квадрат
+
+Console.WriteLine("pishi chislo");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"poluchaetsa {number} v kvadrate = {square}");
+//какая-то конкатенация
