37a2581e462c58d2dbad4e6042dec51b4f08ea3d
Eugene <zh-gen@mail.ru>
2024-02-06 15:51:43 +0300

    added number writing console

--- task002/task002.cs
+++ task002/task002.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// тут надо проверить, насколько число a является квадратом числа b
+// например a = 25, b = 5 -> да
+
+Console.WriteLine("напиши число a");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("напиши число b");
+int numberb = Convert.ToInt32(Console.ReadLine());
