6af155250fa934a00095c87b60919a24b73427cc
Eugene <zh-gen@mail.ru>
2024-02-06 14:35:32 +0300

    added command

--- task001/Program.cs
+++ task001/Program.cs
@@ -2,6 +2,7 @@
 //  1. На вход принимаем число
 //  2. Возводим в квадрат
 
+Console.WriteLine("pishi chislo");
 int number = Convert.ToInt32(Console.ReadLine());
 int square = number * number;
 Console.WriteLine(square);
