988ef5d82a9d930d533d92f965a36e556ce80b79
Eugene <zh-gen@mail.ru>
2024-02-06 16:43:05 +0300

    added full week names

--- task003/days.cs
+++ task003/days.cs
@@ -3,3 +3,22 @@
 
 Console.WriteLine("пиши номер дня, я скажу, как он называется");
 int day = Convert.ToInt32(Console.ReadLine());
+
+string message = "ты же знаешь, сколько дней недели, верно?";
+if (day==1) {message = "monday";
+}
+if (day==2) {message = "tuesday";
+}
+if (day==3) {message = "wednesday";
+}
+if (day==4) {message = "thursday";
+}
+if (day==5) {message = "friday";
+}
+if (day==6) {message = "saturday";
+}
+if (day==7) {message = "sunday";
+}
+else {message = message;};
+
+Console.WriteLine (message);
