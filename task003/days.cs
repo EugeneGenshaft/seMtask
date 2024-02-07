228e3973b7883c39f07da69e3fedc43abc7519e4
Eugene <zh-gen@mail.ru>
2024-02-07 18:03:13 +0300

    added full week names but without string

--- task003/days.cs
+++ task003/days.cs
@@ -4,21 +4,34 @@
 Console.WriteLine("пиши номер дня, я скажу, как он называется");
 int day = Convert.ToInt32(Console.ReadLine());
 
-string message = "ты же знаешь, сколько дней недели, верно?";
-if (day==1) {message = "monday";
-}
-if (day==2) {message = "tuesday";
-}
-if (day==3) {message = "wednesday";
-}
-if (day==4) {message = "thursday";
-}
-if (day==5) {message = "friday";
-}
-if (day==6) {message = "saturday";
-}
-if (day==7) {message = "sunday";
-}
-else {message = message;};
 
-Console.WriteLine (message);
+if (day==1) {Console.WriteLine("monday");
+}
+else if (day==2) 
+{
+    Console.WriteLine("tuesday");
+    }
+else if (day==3) 
+{
+    Console.WriteLine("wednesday");
+    }
+    else if (day==4) 
+{
+    Console.WriteLine("thursday");
+    }
+    else if (day==5) 
+{
+    Console.WriteLine("friday");
+    }
+    else if (day==6) 
+{
+    Console.WriteLine("saturday");
+    
+    }
+    else if (day==7) 
+{
+    Console.WriteLine("sunday");
+    }
+ else
+ {Console.WriteLine ("ты же знаешь, сколько дней недели, верно?");};
+
