2799935da05a6f489e0ca08934911f9ee598a3b4
Eugene <zh-gen@mail.ru>
2024-02-09 16:27:33 +0300

    found the last number

--- task005/find last number.cs
+++ task005/find last number.cs
@@ -1,3 +1,20 @@
 ﻿//тут надо найти последнюю цифру трехзначного числа
 
 Console.WriteLine("пиши трехзначное число, гений");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    number *= -1;
+}
+
+if (number >= 100 && number <=999)
+{
+    int result = number % 10;
+    Console.WriteLine ($"последняя цифра: {result}");
+}
+else 
+{
+    Console.WriteLine("are u serious?");
+}
