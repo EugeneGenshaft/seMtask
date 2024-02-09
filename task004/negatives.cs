1999ca77d9605dd5b6a8cd58eb89089071bf604f
Eugene <zh-gen@mail.ru>
2024-02-09 11:44:07 +0300

    made negative to positive counts

--- task004/negatives.cs
+++ task004/negatives.cs
@@ -1,3 +1,12 @@
 ﻿// тут надо сделать так, чтобы программа в ответ на число N
 // выдывало числf от -N до N (5-> -5.-4....5)
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Пиши число, сейчас покажу че");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = -number; i <= number;i++) {
+    Console.Write($"{i}, ");
+}
+
+int count = -number;
